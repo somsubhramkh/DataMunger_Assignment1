c854ad7b57c045119df6071610cf857513e4a8ea
manojkumar3036 <shah.manoj86@gmail.com>
2017-08-09 19:00:43 +0530

    Done with where clause and multiple where clause

--- src/com/stackroute/datamunger/DataMunger.java
+++ src/com/stackroute/datamunger/DataMunger.java
@@ -61,8 +61,18 @@ public class DataMunger {
 
 	// get and display the where conditions part(if where condition exists)
 	public String getConditionsPartQuery(String queryString) {
-
-		return null;
+		queryString = queryString.toLowerCase();
+		String whereClause=null;
+		if(queryString.contains("where"))
+		{
+			whereClause=queryString.split("where\\s+")[1].split("group by")[0].split("order by")[0];
+			return whereClause;
+		}
+		else
+		{
+			return null;
+		}
+		
 
 	}
 
@@ -72,11 +82,44 @@ public class DataMunger {
 	 */
 	public String[] getConditions(String queryString) {
 
-		return null;
+		queryString = queryString.toLowerCase();
+		String whereClause=null;
+		String [] propertyNameAndValue;
+		String propertyName;
+		String propertyValue;
+		String conditionalOperator;
+		if(queryString.contains("where"))
+		{	
+			// where clause
+			whereClause=queryString.split("where\\s+")[1].split("group by")[0].split("order by")[0];
+			// parsing the where clause basis on and | or
+			String [] logicalExpressions=whereClause.split("\\s+or\\s+|\\s+and\\s+");
+			for(String expression:logicalExpressions)
+			{
+				expression=expression.trim();			
+				propertyNameAndValue=expression.split("<=|>=|!=|<|>|=");
+				propertyName=propertyNameAndValue[0].trim();
+				//System.out.println(propertyName);
+				propertyValue=propertyNameAndValue[1].trim();
+				//System.out.println(propertyValue);
+				conditionalOperator = expression.split(propertyName)[1].trim().split(propertyValue)[0];
+				//System.out.println(conditionalOperator);	
+				
+			}
+			
+			return logicalExpressions;
+			
+		}
+		else
+		{
+			return null;
+		}
 	}
 
 	// get the logical operators(applicable only if multiple conditions exist)
 	public String[] getLogicalOperators(String queryString) {
+		
+		
 
 		return null;
 
@@ -95,16 +138,33 @@ public class DataMunger {
 	// get order by fields if order by clause exists
 	public String[] getOrderByFields(String queryString) {
 		queryString = queryString.toLowerCase();
-		String [] orderByFields=queryString.split("order by")[1].split("[\\s,]+");
-		return orderByFields;
+		if(queryString.contains("order by"))
+		{
+			String [] orderByFields=queryString.split("order by")[1].split("[\\s,]+");
+			return orderByFields;
+		}
+		else
+		{
+			return null;
+		}
+		
 	}
 
 	// get group by fields if group by clause exists
 	public String[] getGroupByFields(String queryString) {
 		queryString = queryString.toLowerCase();
-		String [] groupByFields=queryString.split("group by\\s+")[1].split("[\\s,]+");	
+		if(queryString.contains("group by"))
+		{
+			String [] groupByFields=queryString.split("group by\\s+")[1].split("[\\s,]+");	
+			return groupByFields;
+		}
+		else
+		{
+			return null;
+		}
+		
 
-		return groupByFields;
+	
 	}
 
 	// parse and display aggregate functions(if applicable)
